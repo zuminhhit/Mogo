6b8e379db204c5d9a22edf396c4e7a5526855f91
ZuMinhh <vuminhhdepzai@gmail.com>
2026-02-20 19:32:34 +0700

    (feat) Add Footer

--- js/main.js
+++ js/main.js
@@ -4,4 +4,11 @@ import initHero from "./modules/hero.js";
 document.addEventListener("DOMContentLoaded", () => {
   initHeader();
   initHero();
+  getCurrentYear();
 });
+
+function getCurrentYear() {
+  const currentYearElement = document.getElementById("current-year");
+  const currentYear = new Date().getFullYear();
+  currentYearElement.textContent = currentYear;
+}
